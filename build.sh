105fe8d1695e9f0df94e5e1e94a22aa98b2db470
Jason Weirather <jason-weirather@users.noreply.github.com>
2017-11-03 10:09:07 -0400

    try to make an executable

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p $PREFIX/opt
 cp $SRC_DIR/bin/muTect-1.1.4.jar $PREFIX/opt/muTect-1.1.4.jar
-alias  mutect="java -Xmx2g -jar $PREFIX/opt/muTect-1.1.4.jar"
-
+cp $SRC_DIR/bin/mutect $PREFIX/bin/mutect
