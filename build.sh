22e220b6372292c8cfb9d7071b0448ce5841a3ce
Jason Weirather <jason-weirather@users.noreply.github.com>
2017-11-03 09:58:04 -0400

    loose some weight

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p $PREFIX/bin
-cp $SRC_DIR/bin/mutect-1.1.4.jar $PREFIX/bin/mutect-1.1.4.jar
-alias  mutect="java -Xmx2g -jar $PREFIX/bin/mutect-1.1.4.jar"
+mkdir -p $PREFIX/opt
+cp $SRC_DIR/bin/muTect-1.1.4.jar $PREFIX/opt/muTect-1.1.4.jar
+alias  mutect="java -Xmx2g -jar $PREFIX/opt/muTect-1.1.4.jar"
 
