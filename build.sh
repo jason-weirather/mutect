cc90b7a05db3e63ab616778ef8de4183acc420d9
Jason Weirather <jason-weirather@users.noreply.github.com>
2017-11-06 13:20:47 -0500

    update version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p $PREFIX/opt
-cp $SRC_DIR/bin/muTect-1.1.4.jar $PREFIX/opt/muTect-1.1.4.jar
+cp $SRC_DIR/bin/muTect-1.1.6.jar $PREFIX/opt/muTect-1.1.6.jar
 cp $SRC_DIR/bin/mutect $PREFIX/bin/mutect
