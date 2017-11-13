731b221839a5a995bb9b5fd0886afba27b2a3790
Jason L Weirather <jason-weirather@users.noreply.github.com>
2017-11-13 10:16:15 -0500

    use a jar file instead

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p $PREFIX/opt
-cp $SRC_DIR/bin/muTect-1.1.4.jar $PREFIX/opt/muTect-1.1.4.jar
+mkdir -p $PREFIX/jar
+cp $SRC_DIR/bin/muTect-1.1.4.jar $PREFIX/jar/muTect-1.1.4.jar
 cp $SRC_DIR/bin/mutect $PREFIX/bin/mutect
